4e7276282071ebcf90e3339d04f21537267b590c
katsuki.iwamoto <43980437+ka2ki@users.noreply.github.com>
2022-02-15 15:17:17 +0900

    UrlParameter

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,15 +1,16 @@
-import { Page2 } from "../Page2";
-import { UrlParameter } from "../UrlParameter";
+import { useParams } from "react-router-dom";
 
-export const Page1Routes = [
-  {
-    path: "/",
-    exact: true,
-    children: <Page2 />
-  },
-  {
-    path: "/:id",
-    exact: false,
-    children: <UrlParameter />
-  }
-];
+export const UrlParameter = () => {
+  /** パラメーター値の扱い（useParams()）
+   * page2Routes.jsxでパラメーター値を
+   * path: "/:id" と設定しているので
+   * id としてを受け取ることができる
+   */
+  const { id } = useParams();
+  return (
+    <div>
+      <h1>UrlParameterページです</h1>
+      <p>パラメーターは {id} です</p>
+    </div>
+  );
+};
