394b7225d45827bd36d65c2fd4a7f82a7dca6b08
katsuki.iwamoto <43980437+ka2ki@users.noreply.github.com>
2022-02-16 12:16:09 +0900

    stateを渡すページ遷移

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  // ex)ルーティング時にstateの情報を渡す
+  const arr = [...Array(100).keys()];
+
   return (
     <div>
       <h1>Page1ページです</h1>
-      <Link to="/page1/detailA">DetailA</Link>
+      {/* ルーティングのときに state 情報も渡すと
+       * 情報を持ったままルーティングさせることができる
+       */}
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
     </div>
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,4 +1,12 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  /** useLocation()
+   * ルーティングの際に設定したstateの情報を受け取る
+   */
+  const { state } = useLocation();
+  console.log(state);
+
   return (
     <div>
       <h1>Page1DetailAページです</h1>
